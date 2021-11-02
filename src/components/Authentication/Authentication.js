d7e3276902a7b7f1e7b982bf5011c6f2fe3dc41b
Ewe <ewelina.polewka1@gmail.com>
2021-11-02 19:15:24 +0100

    Scss structure added

---
+++ src/components/Authentication/Authentication.js
@@ -0,0 +1,16 @@
+import "./Authentication.scss";
+
+import React from 'react';
+
+const Authentication = () => {
+    return (
+        <div>
+            <form action="" className="auth__form">
+                <input type="text"/>
+                <input type="text"/>
+            </form>
+        </div>
+    );
+};
+
+export default Authentication;
---
+++ src/components/Collections/Collections.js
@@ -0,0 +1,13 @@
+import "./Collections.scss";
+
+import React from 'react';
+
+const Collections = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Collections;
