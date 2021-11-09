88004e8ae7f3dc5433e2350988fac53613486e34
Ewe <ewelina.polewka1@gmail.com>
2021-11-09 14:44:20 +0100

    Icon credits added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ function App() {
         </AuthProvider>
         {/*<Collections/>*/}
         {/*<NewCollection/>*/}
-        <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        {/*<small className="icons-credits">Icons made by*/}
+        {/*    <a href="https://www.freepik.com" title="Freepik"> Freepik </a>*/}
+        {/*    from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>*/}
+        {/*</small>*/}
     </div>
   );
 }
