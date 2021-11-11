c2c2de9088c4a5c5c43fbb7eb2752888c46672a7
Ewe <ewelina.polewka1@gmail.com>
2021-11-11 15:41:21 +0100

    Generating collection titles from database

--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import "./Collections.scss";
 import trash from "./../../images/delete.png";
+import  { db } from "../../firebase";
 
 const Collections = () => {
+    const [cardTitles, setCardTitles] = useState([]);
+
+    useEffect(() => {
+        db.collection("flashcards-collection").onSnapshot((snapshot) => {
+            setCardTitles(
+                snapshot.docs.map((doc) => doc.data().title)
+            );
+        });
+    }, []);
+
     return (
         <>
             <div className="collection">
@@ -14,20 +25,15 @@ const Collections = () => {
                         </div>
                     </div>
 
-                    <div className="collection__single">
-                        <div className="collection__card">
-                            <img src={trash} className="collection__remove" alt="trash icon" />
-                            <span>React</span>
+                {cardTitles.map(title => {
+                    return <div className="collection__single" key={cardTitles.id}>
+                                <div className="collection__card">
+                                    <img src={trash} className="collection__remove" alt="trash icon" />
+                                    <span>{title}</span>
+                                </div>
+                            <div className="collection__frame frame"/>
                         </div>
-                        <div className="collection__frame frame"/>
-                    </div>
-                    <div className="collection__single">
-                        <div className="collection__card">
-                            <img src={trash} className="collection__remove" alt="trash icon" />
-                            <span>Sass</span>
-                        </div>
-                        <div className="collection__frame frame"/>
-                    </div>
+                    })}
             </div>
         </>
     );
